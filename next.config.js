ffa79083ad52807c9b041c0c0e35a55ffe1e0f97
Douglas Campos <douglas14@hotmail.com>
2023-09-13 17:17:20 -0400

    added dockerfile to next

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['housefy-dev-bucket.s3.amazonaws.com'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  output: 'standalone',
 }
-
 module.exports = nextConfig
