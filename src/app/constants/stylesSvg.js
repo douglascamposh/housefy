3b7af4bce765c1616ba793562cd41de215273cf6
obed-tc <castroobed53@gmail.com>
2023-10-20 07:09:10 -0400

    Added reserved properties status color.

--- src/app/constants/stylesSvg.js
+++ src/app/constants/stylesSvg.js
@@ -1,4 +1,3 @@
-
 export const stylesSvg = `
 .default-path-style {
   fill: #313231;
@@ -14,24 +13,30 @@ export const stylesSvg = `
   fill: #2d0df1;
 }
 .selected-path:hover {
-  fill: #2d0df1;
+  fill: #5E46EE;
 }
 .not-available-path {
   fill: #e90107;
+}
+.not-available-path:hover {
+  fill: #EE474B;
 }
 .common-area-path {
   fill: #66FFFF;
 }
 .common-area-path:hover {
-  fill: #66FFFF;
+  fill: #47EEEE;
 }
-.not-available-path:hover {
-  fill: #e90107;
+.reserved-path {
+  fill: #ECC604;
+}
+.reserved-path:hover {
+  fill: #EBD047;
 }
 .available-path {
   fill: #2db303;
 }
 .available-path:hover {
-  fill: #2db303;
+  fill: #6FEE47;
 }
 `;
