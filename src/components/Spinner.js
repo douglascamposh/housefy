4c7bb84e0283ba38b0424a2bd7adeaf7b3537df4
obed-tc <castroobed53@gmail.com>
2023-11-07 17:52:56 -0400

    Modified loading view

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="flex justify-center items-center h-full">
-      <div className="border-t-4 border-blue-800 border-solid rounded-full animate-spin w-12 h-12"></div>
+    <div className="flex items-center justify-center h-screen">
+      <div className="loader"></div>
     </div>
   );
 };
